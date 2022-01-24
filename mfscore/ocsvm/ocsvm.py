d2f65733f1f9abefe0a0e24f813c267558bebec3
Grzegorz Skoraczynski <gskoraczynski@gmail.com>
2022-01-24 18:34:57 +0100

    Packaged

---
+++ mfscore/ocsvm/ocsvm.py
@@ -0,0 +1,4 @@
+print("Hello world")
+
+def f():
+    return 3
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MF-Score',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Some ML scores for scoring some molecules.'
+    author='Grzegorz Skoraczynski',
+    license='MIT',
+)
