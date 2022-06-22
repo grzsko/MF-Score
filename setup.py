1c75d513fb42a234ca3237a7653e22d32a60e411
Grzegorz Skoraczynski <gskoraczynski@gmail.com>
2022-06-22 19:03:29 +0200

    Sklearn version actualized

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     description='Some ML scores for scoring some molecules.',
     author='Grzegorz Skoraczynski',
     license='MIT',
-    install_requires=["scikit-learn", "numpy"]
+    install_requires=["scikit-learn>=0.24.2", "numpy"]
 )
